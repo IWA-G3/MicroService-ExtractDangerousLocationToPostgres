a2aa6c3d29e76dbb03307fb258b9d1ccc3db3e2a
Adrien Alexandre <alexandreadrien@orange.fr>
2020-11-21 14:50:42 +0100

    init project

---
+++ src/main/java/polytech/group3/iwa/ExtractDangerousLocationToPostgresApplication.java
@@ -0,0 +1,13 @@
+package polytech.group3.iwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractDangerousLocationToPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractDangerousLocationToPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/polytech/group3/iwa/ExtractDangerousLocationToPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.group3.iwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractDangerousLocationToPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
